0033d5c7b20eec430f7479e73d5656f40d0a362b
Brady <bradytwiz@gmail.com>
2025-10-07 18:21:17 -0700

    Adding in project 1 as a starting point

---
+++ index.js
@@ -0,0 +1,70 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+let posts = {}
+let runningId = 0
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    const postsArray = Object.values(posts);
+    const sortedPosts = postsArray.sort((a, b) => b.timestamp - a.timestamp);
+
+    res.render("index.ejs", {sortedPosts});
+
+});
+
+app.get('/post', (req, res) => {
+    res.render("post.ejs", { post: null, editMode: false });
+});
+
+app.get("/edit/:id", (req, res) => {
+    const { id } = req.params;
+    const post = posts[id];
+
+    res.render("post.ejs", { post, editMode: true });
+});
+
+app.post("/submit", (req, res) => {
+    const {name, title, content} = req.body
+    posts[runningId] = {
+        id: runningId,
+        name,
+        title,
+        content,
+        timestamp: new Date()
+    };
+    runningId++;
+    res.redirect("/");
+});
+
+app.delete("/posts/:id", (req, res) => {
+    const { id } = req.params;
+    delete posts[id]; 
+});
+
+app.patch("/posts/:id", (req, res) => {
+    const { id } = req.params;
+    const { title, content, name } = req.body;
+
+    const post = posts[id];
+
+    if (title !== undefined) post.title = title;
+    if (content !== undefined) post.content = content;
+    if (name !== undefined) post.name = name;
+
+    post.timestamp = new Date();
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
